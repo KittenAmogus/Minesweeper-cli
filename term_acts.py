99c09a30de38ba4f786df265e23cafb277efca3e
KittenAmogus <kittenamogus@gmail.com>
2025-11-26 12:43:27 +0300

    Windows: add arrows support

--- term_acts.py
+++ term_acts.py
@@ -10,6 +10,9 @@ class Getch:
 		except ImportError:
 			self.impl = _GetchUnix()
 	
+	def getBytes(self):
+		return self.impl.getBytes()
+
 	def __call__(self):
 		return self.impl()
 
@@ -18,17 +21,37 @@ class _GetchWindows:
 	def __init__(self):
 		import msvcrt
 	
+	def getBytes(self):
+		import msvcrt
+		return msvcrt.getch()
+
+	def _getArrow(self, char):
+		match (char):
+			case "H":
+				return "A"
+			case "K":
+				return "D"
+			case "P":
+				return "B"
+			case "M":
+				return "C"
+			case _:
+				return char
+
 	def __call__(self):
 		import msvcrt
 		try:
 			return msvcrt.getch().decode('utf-8')
 		except UnicodeDecodeError:
-			return '\x03'
+			return self._getArrow(msvcrt.getch().decode('utf-8'))
 
 
 class _GetchUnix:
 	def __init__(self):
 		import tty, sys
+	
+	def getBytes(self):
+		return bytes(self())
 
 	def __call__(self):
 		import sys, tty, termios
@@ -42,4 +65,13 @@ class _GetchUnix:
 			termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 		
 		return ch
+
+
+if __name__ == "__main__":
+	getch = Getch()
+	while True:
+		char = getch.getBytes()
+		if char == b'\x03':
+			break
+		print(repr(char))
 
